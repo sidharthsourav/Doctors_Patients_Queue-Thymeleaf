97612e303acef659d63b6e1ec30561598b50bf73
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2022-02-23 23:39:14 +0530

    final attributes added and configuration file done.

--- src/main/java/com/example/demo/model/Patient.java
+++ src/main/java/com/example/demo/model/Patient.java
@@ -10,85 +10,108 @@ public class Patient {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	String date,name,problem;
-	Integer id,age,notAttendedyet,seen,updated,cancelled;
+	String date,name,phoneNo;
+	Integer age;
+	Integer id,notAttendedyet,seen,updated,cancelled;
 	
-	public Patient() {
+	public Patient()
+	{
 		
 	}
-	public Patient(String date, String name, String problem, Integer id, Integer age, Integer notAttendedyet,
+
+	public Patient(String date, String name, String phoneNo, Integer age, Integer id, Integer notAttendedyet,
 			Integer seen, Integer updated, Integer cancelled) {
 		super();
 		this.date = date;
 		this.name = name;
-		this.problem = problem;
-		this.id = id;
+		this.phoneNo = phoneNo;
 		this.age = age;
+		this.id = id;
 		this.notAttendedyet = notAttendedyet;
 		this.seen = seen;
 		this.updated = updated;
 		this.cancelled = cancelled;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getProblem() {
-		return problem;
+
+	public String getPhoneNo() {
+		return phoneNo;
 	}
-	public void setProblem(String problem) {
-		this.problem = problem;
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+
 	public Integer getAge() {
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public Integer getNotAttendedyet() {
 		return notAttendedyet;
 	}
+
 	public void setNotAttendedyet(Integer notAttendedyet) {
 		this.notAttendedyet = notAttendedyet;
 	}
+
 	public Integer getSeen() {
 		return seen;
 	}
+
 	public void setSeen(Integer seen) {
 		this.seen = seen;
 	}
+
 	public Integer getUpdated() {
 		return updated;
 	}
+
 	public void setUpdated(Integer updated) {
 		this.updated = updated;
 	}
+
 	public Integer getCancelled() {
 		return cancelled;
 	}
+
 	public void setCancelled(Integer cancelled) {
 		this.cancelled = cancelled;
 	}
+
 	@Override
 	public String toString() {
-		return "Patient [date=" + date + ", name=" + name + ", problem=" + problem + ", id=" + id + ", age=" + age
+		return "Patient [date=" + date + ", name=" + name + ", phoneNo=" + phoneNo + ", age=" + age + ", id=" + id
 				+ ", notAttendedyet=" + notAttendedyet + ", seen=" + seen + ", updated=" + updated + ", cancelled="
 				+ cancelled + "]";
 	}
+	
 	
 	
 }
