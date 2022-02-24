e6394b650e84a9b052aba9552d6657d4b05abc6a
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2022-02-24 14:09:16 +0530

    controller updated.

--- src/main/java/com/example/demo/controller/PatientController.java
+++ src/main/java/com/example/demo/controller/PatientController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
-public class PatientController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.services.PatientService;
+
+@Controller
+public class PatientController {
+	
+	@Autowired
+	private PatientService patientService;
+	
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("patientList",patientService.getallpatients() );
+		return "index";
+	}
 }
